e98ae6993385d4460eab926cc929c530333aba85
Oliwier601 <113879766+Oliwier601@users.noreply.github.com>
2022-12-05 09:23:03 +0100

    Create Asocjacja.cs

---
+++ Asocjacja.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asocjacja
+{
+    class account
+    {
+        private string emailAddress;
+        private string name;
+        public string getEmailAddress()
+        {
+
+        }
+        public string getName()
+        {
+
+        }
+        public void setEmailAddress(string emailAddress)
+        {
+
+        }
+        public void setName(string name)
+        {
+
+        }
+    }
+    class contact
+    {
+        private string emailAddress;
+        private string faxNumber;
+        private string name;
+        private string primaryContactMethod;
+    }
+    class address_book
+    {
+        private string name;
+    }
+    class contrct_group
+    {
+        private string name;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
